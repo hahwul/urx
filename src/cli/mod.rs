5f8e0013ce25dc19700fd2e164781b4a4529c8d9
HAHWUL <hahwul@gmail.com>
2025-04-17 00:17:39 +0900

    Add help_heading for --include-robots flag

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -52,6 +52,11 @@ pub struct Args {
     #[clap(long)]
     pub urlscan_api_key: Option<String>,
 
+    #[clap(help_heading = "Provider Options")]
+    /// Include robots.txt discovery
+    #[clap(long)]
+    pub include_robots: bool,
+
     #[clap(help_heading = "Display Options")]
     /// Show verbose output
     #[clap(short, long)]
@@ -176,11 +181,6 @@ pub struct Args {
     #[clap(help_heading = "Testing Options")]
     #[clap(long, alias = "es", visible_alias = "--es")]
     pub exclude_status: Vec<String>,
-
-    /// Include robots.txt discovery
-    #[clap(long)]
-    pub include_robots: bool,
-
 
     /// Extract additional links from collected URLs (requires HTTP requests)
     #[clap(help_heading = "Testing Options")]
